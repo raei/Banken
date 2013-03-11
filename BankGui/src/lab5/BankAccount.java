072ef13f6a8a67d807b87496706691f3160cb8e5
raei <raei@DS0812325.sandviken.local>
2013-03-11 08:46:08 +0100

    V1.5.0

---
+++ BankGui/src/lab5/BankAccount.java
@@ -0,0 +1,61 @@
+package lab5;
+
+
+
+public class BankAccount {
+    
+    //instansvaribler
+    private String ownerName; 
+    private int balance;
+    
+    //konstruktor
+    public BankAccount(String ownerName)
+    {
+        this.ownerName = ownerName;
+        this.balance = 0;        
+    }
+    
+    public BankAccount(String ownerName, int balance)
+    {
+        this.ownerName = ownerName;
+        this.balance = balance;        
+    }//end constructors
+    
+    //metoder
+    public boolean withdraw(int withdraw){
+        boolean koll = false;
+        if(withdraw <= this.balance){
+            this.balance = this.balance - withdraw;
+            koll = true;
+        }   
+        return koll;        
+    }//end withdraw     
+    
+    public boolean deposit(int deposit){
+        boolean koll = false;
+        if(deposit > 0){
+            this.balance = this.balance + deposit;
+            koll = true;
+        }
+        return koll;
+    }//end deposit
+    
+    public void setOwnerName(String name){
+        this.ownerName = name;
+    }//end setOwnerName
+     
+    public String getOwnerName(){
+        return this.ownerName;
+    }//end getOwnername
+    
+    public int getBalance(){
+        return  this.balance;
+    }
+    
+    @Override
+    public String toString(){
+        return "Innehavare: " + this.ownerName + "\n" + 
+                "Balance: " + this.balance + "\n";
+    }//end toString
+}//end class
+
---
+++ BankGui/src/lab5/BankGUI.java
@@ -0,0 +1,38 @@
+package lab5;
+
+
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+
+public class BankGUI extends JFrame{
+    
+    private JTabbedPane tabPane;
+    private AddAccount addAccountPanel;
+    private SearchAccount searchAccountPanel;
+    
+    
+    public BankGUI() {        
+        tabPane = new JTabbedPane();
+        addAccountPanel = new AddAccount();
+        searchAccountPanel = new SearchAccount();        
+        tabPane.add("Add account",addAccountPanel);
+        tabPane.add("Search account", searchAccountPanel);        
+        
+        final int nTabIndex = tabPane.indexOfTabComponent( tabPane );
+        final boolean bIsVisible = tabPane.isEnabledAt( 1);
+        if ( bIsVisible== false ) {
+            // Do stuff with myPanel
+            this.repaint();
+        }
+        this.add(tabPane);       
+    }//end konstruktor
+    
+    public static void main(String[] args) {
+        BankGUI frame = new BankGUI();
+        frame.setSize(800, 600);
+        frame.setTitle("Handelsbanken");
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(BankGUI.EXIT_ON_CLOSE);
+        frame.setVisible(true);       
+    }//end main    
+}//end class
